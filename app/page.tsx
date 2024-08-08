d1a494055f255d166c50d3b70863a779ef89cc05
4rjunc <arjunmundamani@gmail.com>
2024-08-08 14:50:54 +0530

    Fixed wallet connection

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
-import WalletConnect from "./components/WalletConnect";
+import Connect from "./components/Connect";
+import styles from "./page.module.css";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <WalletConnect />
-      Hello World
+    <main className={styles["main"]}>
+      <Connect />
     </main>
   );
 }
