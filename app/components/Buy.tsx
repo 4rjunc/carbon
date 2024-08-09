a0a5318b29e234383e72a9f0b6ce700f64995025
4rjunc <arjunmundamani@gmail.com>
2024-08-09 23:05:26 +0530

    Fixing paperfectch

--- app/components/Buy.tsx
+++ app/components/Buy.tsx
@@ -1,6 +1,12 @@
 // create an infinite scroll to item display
 // when item is clicked openup a modal to pay -> if payment is succesfull -> download the pdf from the web3storage
 
+import { useState, useEffect } from "react";
+//abi
+import CarbonMarketplace from "../contract/CarbonMarketplace.json";
+import { ethers } from "ethers";
+import { useEthersSigner } from "../config/ether";
+
 //ui components
 import {
   Card,
@@ -10,11 +16,50 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
-//buyhandler
-import BuyButton from "./BuyButton";
+const CONTRACT_ADDRESS = "0xd369a1d59a375E7771c1A958e2Bf5b6bC0fFAE5D";
 
 const Buy = () => {
+  const signer = useEthersSigner();
+  const contract = new ethers.Contract(
+    CONTRACT_ADDRESS,
+    CarbonMarketplace,
+    signer,
+  );
+
+  const [allPapers, setAllPapers] = useState([]);
+  console.log("contract:", contract, "signer", signer);
+  const handleFetch = async () => {
+    try {
+      const [papers, tokenIds] = await contract.getAllPapers();
+      setAllPapers(
+        papers.map((paper: any, index: any) => ({
+          ...paper,
+          tokenId: tokenIds[index].toString(),
+        })),
+      );
+    } catch (error) {
+      console.error("Error loading all papers:", error);
+    } finally {
+      console.log("papers", allPapers);
+    }
+  };
+
+  useEffect(() => {
+    handleFetch();
+  }, []);
+
   return (
     <div>
       <p className="text-xl font-semibold font-[#403d39]">Explore knowledge</p>
@@ -32,7 +77,34 @@ const Buy = () => {
             </p>
           </CardContent>
           <CardFooter>
-            <BuyButton />
+            <div className="bg-[#eb5e28] rounded-md py-1 px-3">
+              <AlertDialog>
+                <AlertDialogTrigger className="text-white">
+                  Buy 🎉
+                </AlertDialogTrigger>
+                <AlertDialogContent>
+                  <AlertDialogHeader>
+                    <AlertDialogTitle>
+                      Are you absolutely sure?
+                    </AlertDialogTitle>
+                    <AlertDialogDescription>
+                      This action cannot be undone.
+                    </AlertDialogDescription>
+                  </AlertDialogHeader>
+                  <AlertDialogFooter>
+                    <AlertDialogCancel className="hover:bg-[#CCC5B9]">
+                      Cancel ❌{" "}
+                    </AlertDialogCancel>
+                    <AlertDialogAction
+                      //onClick={handleBuy}
+                      className="hover:bg-[#eb5e28] "
+                    >
+                      Confirm Order ✅{" "}
+                    </AlertDialogAction>
+                  </AlertDialogFooter>
+                </AlertDialogContent>
+              </AlertDialog>
+            </div>
           </CardFooter>
         </Card>
       </div>
--- app/components/BuyButton.tsx
+++ app/components/BuyButton.tsx
@@ -19,31 +19,6 @@ import {
 } from "@/components/ui/alert-dialog";
 
 const BuyButton = () => {
-  const signer = useEthersSigner();
-  const contract = new ethers.Contract(
-    "0xa5752293C773E95dA197dF38D17381b733c8A087",
-    CarbonMarketplace,
-    signer,
-  );
-
-  const [allPapers, setAllPapers] = useState([]);
-  console.log("contract:", contract, "signer", signer);
-  const handleBuy = async () => {
-    try {
-      const [papers, tokenIds] = await contract.getAllPapers();
-      setAllPapers(
-        papers.map((paper: any, index: any) => ({
-          ...paper,
-          tokenId: tokenIds[index].toString(),
-        })),
-      );
-    } catch (error) {
-      console.error("Error loading all papers:", error);
-    } finally {
-      console.log("papers", allPapers);
-    }
-  };
-
   return (
     <div className="bg-[#eb5e28] rounded-md py-1 px-3">
       <AlertDialog>
